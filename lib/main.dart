ef1eba2be0f3b8e7052a660472872924616c7502
Gabriel Feitosa <63379991+gabrielfeitosac@users.noreply.github.com>
2025-07-06 23:53:13 -0300

    fix: ajuste de carregamento do app

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:fast2mine_challenge/core/utils/colors.dart';
 import 'package:fast2mine_challenge/core/utils/device_screen.dart';
 import 'package:fast2mine_challenge/features/home_page/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,14 +17,34 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    bool firstFrameAllowed = false;
     DeviceScreen.setDeviceScreen(context);
-    return MaterialApp(
-      title: 'SIMULACAO',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-      ),
-      home: const HomePage(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            if (MediaQuery.of(context).size == Size.zero) {
+              return const SizedBox();
+            }
+            if (!firstFrameAllowed) {
+              RendererBinding.instance.allowFirstFrame();
+              firstFrameAllowed = true;
+            }
+            DeviceScreen.setDeviceScreen(context);
+            if (constraints.maxWidth == 0 || constraints.maxHeight == 0) {
+              return const SizedBox();
+            }
+            return MaterialApp(
+              title: 'SIMULACAO',
+              debugShowCheckedModeBanner: false,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+              ),
+              home: const HomePage(),
+            );
+          },
+        );
+      },
     );
   }
 }
